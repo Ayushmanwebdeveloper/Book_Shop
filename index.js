577e27f8eec6dd15d7a063857fb22b774c978bb2
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2022-07-05 18:56:36 +0530

    feat:add cart

--- index.js
+++ index.js
@@ -36,7 +36,6 @@ cart.innerHTML = `<ul>
             </table>
             <p class="total-container" id="total-price"></p>
             <a href="#" id="checkout-btn" class="cart-btn">Checkout</a>
-            <a href="#" id="clear-cart" class="cart-btn">Clear Cart</a>
           </div>
         </li>
       </ul>`;
@@ -48,6 +47,7 @@ heading1.id = "heading1";
 body.appendChild(heading1);
 let div2 = document.createElement("div");
 div2.className = "div2";
+div2.id ="grid";
 body.appendChild(div2);
 let arr=[{
     "author": "Douglas Crockford",
@@ -147,4 +147,127 @@ function toggleCart() {
 toggleCartBtn.addEventListener("click", function (e) {
   e.preventDefault();
   toggleCart();
+});
+function getLSContent() {
+  const lsContent = JSON.parse(localStorage.getItem("products")) || [];
+  return lsContent;
+}
+function setLSContent(lsContent) {
+  localStorage.setItem("products", JSON.stringify(lsContent));
+}
+function calculateTotal(prices) {
+  return prices.reduce(function (prev, next) {
+    return prev + next;
+  }, 0);
+}
+function getCartItemPrices() {
+  const prices = [];
+  let nums = cartContent.querySelectorAll("tr td:nth-child(3)");
+  if (nums.length > 0) {
+    for (let cell = 0; cell < nums.length; cell++) {
+      let num = nums[cell].innerText;
+      num = num.replace(/[^\d]/g, "");
+      num = parseFloat(num);
+      prices.push(num);
+    }
+    return prices;
+  } else {
+    return;
+  }
+}
+function displayCartTotal() {
+  const prices = getCartItemPrices();
+  let total = 0;
+  if (prices) {
+    total = calculateTotal(prices);
+    totalPriceContainer.innerHTML = `<span class="total">Total: $${total.toFixed(
+      2
+    )}</span>`;
+  } else {
+    totalPriceContainer.innerHTML = '<span class="total">Total: $0</span>';
+  }
+}
+function displayProducts() {
+  const lsContent = getLSContent();
+  let productMarkup = "";if (lsContent !== null) {
+    for (let product of lsContent) {
+      productMarkup += `
+          <tr>
+          <td><img class="cart-image" src="${product.image}" alt="${product.name
+        }" width="120"></td>
+          <td>
+            ${product.name}
+          </td>
+          <td>${product.price}</td>
+          <td><a href="#" data-id="${product.id}" class="remove">X</a></td>
+          </tr>
+        `;
+    }
+  } else {
+    productMarkup = "Your cart is empty.";
+  }
+  cartContent.querySelector("tbody").innerHTML = productMarkup;
+}
+function saveProduct(clickedBtn) {
+  const productId = clickedBtn.getAttribute("data-id");
+  const prodImage = arr[productId].imageLink;
+  const prodName = arr[productId].title;
+  const prodPrice = arr[productId].price;
+
+  let isProductInCart = false;
+  const lsContent = getLSContent();
+lsContent.forEach(function (product) {
+    if (product.id === productId) {
+      alert("This Course Is Already Added To Your Cart");
+      isProductInCart = true;
+    }
+  });
+if (!isProductInCart) {
+    lsContent.push({
+      id: (productId),
+      image: prodImage,
+      name: prodName,
+      price: prodPrice
+    });
+setLSContent(lsContent);
+    displayProducts();
+  }
+}
+
+function removeProduct(productId) {
+ const lsContent = getLSContent();
+let productIndex;
+  lsContent.forEach(function (product, i) {
+    if (product.id === productId) {
+      productIndex = i;
+    }
+  });
+lsContent.splice(productIndex, 1);
+  setLSContent(lsContent);
+  displayProducts();
+}
+document.addEventListener("DOMContentLoaded", function (e) {
+  displayProducts();
+  displayCartTotal();
+});
+productsContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("add-to-cart")) {
+    e.preventDefault();
+    const clickedBtn = e.target;
+    saveProduct(clickedBtn);
+  }
+});
+productsContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("add-to-cart")) {
+    displayCartTotal();
+  }
+});
+cartContent.querySelector("tbody").addEventListener("click", function (e) {
+  e.preventDefault();
+  const clickedBtn = e.target;
+  if (e.target.classList.contains("remove")) {
+    const productId = clickedBtn.getAttribute("data-id");
+    removeProduct(productId);
+    displayCartTotal();
+  }
 });
